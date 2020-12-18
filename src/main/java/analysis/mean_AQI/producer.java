f6fdcf9c8d5be6a2b5111be54e0c9737e7f9f5f2
AymericNoel <aym.noel@gmail.com>
2020-12-18 19:18:48 +0100

    add producer for mean_aqi

--- src/main/java/analysis/mean_AQI/producer.java
+++ src/main/java/analysis/mean_AQI/producer.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.*;
@@ -54,104 +55,44 @@ class producer {
 
         Serde<String> stringSerde = Serdes.String();
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, Double> stream = builder.stream(STREAM_APP_2_INPUT);
-
-        // KStream<String, String> airQualityStream = stats
-        //         .selectKey((key, jsonRecordString) -> extract_city(jsonRecordString))
-        //         .map((key, value) -> new KeyValue<>(key, extract_pollution_meter(value)));
-
-        // airQualityStream.to(STREAM_APP_1_OUT, Produced.with(stringSerde, stringSerde));
-        class Tuple2<T1, T2> {
-            public T1 value1;
-            public T2 value2;
-            
-            Tuple2(T1 v1, T2 v2) {
-              value1 = v1;
-              value2 = v2;
-            }
-        }
+        KStream<String, Double> stream = builder.stream(STREAM_APP_2_INPUT);        
 
         KGroupedStream<String, Double> groupedStream =
                 stream.groupByKey();
 
-        KTable<Windowed<String>, Double> timeWindowedAggregatedStream =
-                groupedStream
-                        .windowedBy(TimeWindows.of(Duration.ofMinutes(30)))
-                        
-                        .aggregate(
-                                () -> 0L,
-                                (aggKey, newValue, aggValue) -> aggValue + newValue,
-                                Materialized.<String, Double, WindowStore<Bytes, byte[]>>as("time-windowed-aggregated-stream-store")
-                                        .withValueSerde(Serdes.Double()));
 
-        KTable<Windowed<String>,Tuple2<Long,Double>> countAndSum = 
-                groupedStream
-                        .windowedBy(TimeWindows.of(Duration.ofMinutes(30)))
-                        .aggregate(
-                            new Initializer<Tuple2<Long, Double>>() {
-                                @Override
-                                public Tuple2<Long, Double> apply() {
-                                    return new Tuple2<>(0L, 0.0d);
-                                }
+        KTable<Windowed<String>, CountAndSum> timeWindowedAggregatedStream =
+        groupedStream
+                .windowedBy(TimeWindows.of(Duration.ofMinutes(30)))
+                
+                .aggregate(
+                        () -> new CountAndSum(0L, 0.0),
+                        (aggKey, newValue, aggValue) ->{
+                            aggValue.count=(aggValue.count+1);
+                            aggValue.Sum=aggValue.Sum+newValue;
+                             return aggValue;
                             },
-                            new Aggregator<String, String, Tuple2<Long, Double>>() {
-                                @Override
-                                public Tuple2<Long, Double> apply(final String key, final Long value, final Tuple2<Long, Double> aggregate) {
-                                        ++aggregate.value1;
-                                        aggregate.value2 += value;
-                                        return aggregate;
-                                }
-                            },
-                            new Tuple2Serde());
+                        Materialized.<String,Double,WindowStore<Bytes, byte[]>>as("time-windowed-aggregated-stream-store")
+                        .withValueSerde(CountAndSumSerdes)
+                ); //need to implement custom serde
 
-        KTable<Long, Double> Average =
-        
-                timeWindowedAggregatedStream.mapValues(value -> value / ,
-                                        Materialized.as("average-ratings"));
 
-                                        
-        timeWindowedAggregatedStream.toStream().to("velib-nbfreedocks-count-notifications", Produced.with(WindowedSerdes.timeWindowedSerdeFrom(String.class), Serdes.Long()));
-        // final KTable<Long, CountAndSum> ratingCountAndSum =
-        // ratingsById.aggregate(() -> new CountAndSum(0L, 0.0),
-        //                       (key, value, aggregate) -> {
-        //                         aggregate.setCount(aggregate.getCount() + 1);
-        //                         aggregate.setSum(aggregate.getSum() + value);
-        //                         return aggregate;
-        //                       },
-        //                       Materialized.with(Long(), countAndSumSerde));
-        
-        // KTable<Long, Double> ratingAverage =
-        //         ratingCountAndSum.mapValues(value -> value.getSum() / value.getCount(),
-        //                                 Materialized.as("average-ratings"));
+
+        KTable<Windowed<String>, Double> Average =        
+                timeWindowedAggregatedStream.mapValues(value -> value.sum / value.count);
+
+        Average.toStream().to(STREAM_APP_2_OUT, Produced.with(WindowedSerdes.timeWindowedSerdeFrom(String.class), Serdes.Double()));
         return builder.build();
     }
-
-    // final KStreamBuilder builder = new KStreamBuilder();
-
-    // // first step: compute count and sum in a single aggregation step and emit
-    // // 2-tuples <count,sum> as aggregation result values
-    // final KTable<String, Tuple2<Long, Long>> countAndSum = builder.stream("someInputTopic").groupByKey()
-    //         .aggregate(new Initializer<Tuple2<Long, Long>>() {
-    //             @Override
-    //             public Tuple2<Long, Long> apply() {
-    //                 return new Tuple2<>(0L, 0L);
-    //             }
-    //         }, new Aggregator<String, String, Tuple2<Long, Long>>() {
-    //             @Override
-    //             public Tuple2<Long, Long> apply(final String key, final Long value,
-    //                     final Tuple2<Long, Long> aggregate) {
-    //                 ++aggregate.value1;
-    //                 aggregate.value2 += value;
-    //                 return aggregate;
-    //             }
-    //         }, new Tuple2Serde()); // omitted for brevity
-    // // second step: compute average for each 2-tuple
-    // final KTable<String, Double> average = countAndSum.mapValues(new ValueMapper<Tuple2<Long, Long>, Double>() {
-    //     @Override
-    //     public Double apply(Tuple2<Long, Long> value) {
-    //         return value.value2 / (double) value.value1;
-    //     }
-    // });
     // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Stream+Usage+Patterns
+}
 
+class CountAndSum {
+    public long count;
+    public double sum;
+    
+    CountAndSum(long v1, double v2) {
+      count = v1;
+      sum = v2;
+    }
 }
